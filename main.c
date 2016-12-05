cfac4b59c227abb6035cb82aacc73382b84147a3
caseymeiz <caseymeiz@users.noreply.github.com>
2016-12-04 21:09:46 -0500

    Update main.c

--- main.c
+++ main.c
@@ -4,27 +4,40 @@
 
 #include "main_asm.h" /* interface to the assembly module */
 
-const int HSYNC_PERIOD = 763;
-const int HSYNC_ACTIVE = 50;
-const word ROWS         = 523;
+const unsigned short HSYNC_PERIOD    = 763;
+const unsigned short HSYNC_ACTIVE    = 153;
+const unsigned short ROWS = 523;
 
 word row = 0;
+unsigned int lasth = 0;
 
   
-  
 void interrupt 8 unset_interrupt(){
-  PORTB |= row?0x03:0x02;
-  TFLG1 |= 0x01;
+  //PORTB |= row?0x03:0x02;
+  PORTB  = 0x03;
+  TC0 += HSYNC_PERIOD;
+  TFLG1 = 0x01;       // bset clear the interrupt
 }
 
 
 
 void interrupt 9 hsync_interrupt(){
+  //lasth = TCNT; 
+  /*
+  if(++row == ROWS){
+    row = PORTB = 0x00;     // Bring hysnc and vsync low for new frame
+               
+  } else {
+    PORTB = 0x01;    // Bring hsync low for new row
+  }*/
+  
+  PORTB  = ++row == ROWS?row=0:0x01;
+
+  PTP = 0;              // Bring the color bits low every new row
+
+
   TC1 += HSYNC_PERIOD;
-  TC0 = TC1 - HSYNC_ACTIVE; 
-  PORTB = (++row == ROWS)?(row =0x00):(PORTB & ~0x02); 
-  PTP = 0;
-  TFLG1 |= 0x02;        // bset clear the interrupt
+  TFLG1 = 0x02;        // bset clear the interrupt
 }
 
 
@@ -34,8 +47,8 @@ void sync_init(){
   TSCR2 = 0x00;     // Timer Prescaler
   TIOS = 0x03;      // Enable Input Compare on T 0,1
   TSCR1 = 0x80;     // Enable Timer TCNT
+  TC0 = HSYNC_PERIOD - HSYNC_ACTIVE;  
   TC1 = HSYNC_PERIOD;
-  TC0 = TC1 - HSYNC_ACTIVE;  
   TIE = 0x03;       // Enable TC0 TC1 interrupts
   asm{
     cli
@@ -43,14 +56,62 @@ void sync_init(){
 }
 
 void main(void) {
-  PLL_init();        // set system clock frequency to 24 MHz  
+  unsigned int i;
+  int xpos;
+  word col;
+  word color = 1;
+  //int ypos; count 
+  unsigned long x1sq;
+  unsigned long y1sq;
+  unsigned long x2sq;
+  unsigned long y2sq;
+  
+  PLL_init();        // set system clock frequency to 24 MHz 
   DDRB = 0x03;
-  DDRP = 0xFF;
+  DDRP = 0x07;
   PORTB |= 0x03;
   sync_init();
+  ad0_enable();
+  
+  
   for(;;){
 
-    PTP = (TC1-TCNT)>>6;
+     //PTP = (lasth -TCNT);
+     
+     //PTP = vram[TC1-TCNT%680];
+    for(i = 0; i <0x3FFF; i++){
+      xpos = (TC1-TCNT) ;
+      if(xpos > x1sq && row > y1sq && xpos < x2sq  && row < y2sq){
+        PTP = color;
+      }else{
+        PTP = color >> 3;
+      }
+    }
+    PTP = color >> 3;
+    
+    //x1sq = 512;
+    //y1sq = 512;
+    color += PORTA?0:1;
+    
+    x1sq = ad0conv(7)-200;
+    y1sq = ad0conv(6)-200;
+    
+   // x1sq = x1sq * HSYNC_PERIOD / 1023;
+    x2sq = x1sq + 75;
+    
+    //y1sq = y1sq * 480 / 1023;
+    //y1sq += 23;
+    y2sq = y1sq + 75 ; 
+    
+     
+     
+  
+    /*col = TC1 - TCNT;
+    if( col> 300 && col < 350 && row > 250 && row <300){
+      PTP = 1;
+    } else {
+      PTP = 3 ;
+    }*/
   }
   for(;;);
 }
